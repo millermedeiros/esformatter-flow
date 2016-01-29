f9c24c9574ba658167745903712819370eb6a3db
Miller Medeiros <miller@millermedeiros.com>
2016-01-28 20:42:01 -0800

    allow custom options

--- esformatter-flow.js
+++ esformatter-flow.js
@@ -3,27 +3,36 @@
 
 var tk = require('rocambole-token');
 var ws = require('rocambole-whitespace');
+var defaultsDeep = require('lodash.defaultsdeep');
 
-// TODO: support custom options
-ws.setOptions({
-  'value': ' ',
-  'before': {
-    'GenericTypeAnnotationClosingChevron': 0,
-    'GenericTypeAnnotationOpeningChevron': 0,
-    'NullableTypeAnnotationQuestionMark': 1,
-    'ReturnTypeColon': 0,
-    'TypeAnnotationColon': 0,
-    'UnionTypeAnnotationPipe': 1
-  },
-  'after': {
-    'GenericTypeAnnotationClosingChevron': 1,
-    'GenericTypeAnnotationOpeningChevron': 0,
-    'NullableTypeAnnotationQuestionMark': 0,
-    'ReturnTypeColon': 1,
-    'TypeAnnotationColon': 1,
-    'UnionTypeAnnotationPipe': 1
+
+var defaultOptions = {
+  'whiteSpace': {
+    'value': ' ',
+    'before': {
+      'GenericTypeAnnotationClosingChevron': 0,
+      'GenericTypeAnnotationOpeningChevron': 0,
+      'NullableTypeAnnotationQuestionMark': 1,
+      'ReturnTypeColon': 0,
+      'TypeAnnotationColon': 0,
+      'UnionTypeAnnotationPipe': 1
+    },
+    'after': {
+      'GenericTypeAnnotationClosingChevron': 1,
+      'GenericTypeAnnotationOpeningChevron': 0,
+      'NullableTypeAnnotationQuestionMark': 0,
+      'ReturnTypeColon': 1,
+      'TypeAnnotationColon': 1,
+      'UnionTypeAnnotationPipe': 1
+    }
   }
-});
+};
+
+exports.setOptions = setOptions;
+function setOptions(opts) {
+  var options = defaultsDeep({}, opts.flow, defaultOptions);
+  ws.setOptions(options.whiteSpace);
+}
 
 exports.nodeAfter = formatNode;
 function formatNode(node) {
